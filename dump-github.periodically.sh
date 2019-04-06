095638b748606ab1db578466fafdc6b93024a12d
Stéphane Gully <stephane.gully@gmail.com>
2019-04-06 17:18:49 +0200

    split code

--- dump-github.periodically.sh
+++ dump-github.periodically.sh
@@ -8,42 +8,17 @@
 while true
 do
 
-
-  # cleanup the dereferenced github organizations
-  # since this latest script execution (example: when a docker restart is done) 
-  rm -f /tmp/GITHUB_ORGANIZATION_NEW && touch /tmp/GITHUB_ORGANIZATION_NEW
-  rm -f /tmp/GITHUB_ORGANIZATION_OLD && touch /tmp/GITHUB_ORGANIZATION_OLD
-  for GITHUB_ORGANIZATION in $GITHUB_ORGANIZATIONS
-  do
-    echo $GITHUB_ORGANIZATION >> /tmp/GITHUB_ORGANIZATION_NEW
-  done
-  cd /usr/local/apache2/htdocs/
-  for GITHUB_ORGANIZATION in $(ls -d */ | sed 's#.$##g' | grep -v node_modules)
-  do
-    echo $GITHUB_ORGANIZATION >> /tmp/GITHUB_ORGANIZATION_OLD
-  done
-  # to understand comm -23
-  # see https://stackoverflow.com/questions/11165182/bash-difference-between-two-lists
-  for GITHUB_ORGANIZATION_TOCLEAN in $(comm -23 <(sort /tmp/GITHUB_ORGANIZATION_OLD) <(sort /tmp/GITHUB_ORGANIZATION_NEW))
-  do
-    echo "-> Cleaning old github organization $GITHUB_ORGANIZATION_TOCLEAN [taille = $(du -sh /usr/local/apache2/htdocs/$GITHUB_ORGANIZATION_TOCLEAN | awk '{ print $1 }')]"
-    rm -rf /usr/local/apache2/htdocs/$GITHUB_ORGANIZATION_TOCLEAN
-  done
-
-
-  # create the organization list for the index.html 
-  rm -f /usr/local/apache2/htdocs/GITHUB_ORGANIZATIONS.txt && touch /usr/local/apache2/htdocs/GITHUB_ORGANIZATIONS.txt
-  for GITHUB_ORGANIZATION in $GITHUB_ORGANIZATIONS
-  do
-    echo $GITHUB_ORGANIZATION >> /usr/local/apache2/htdocs/GITHUB_ORGANIZATIONS.txt
-    mkdir -p /usr/local/apache2/htdocs/$GITHUB_ORGANIZATION/
-    cp -f /usr/local/apache2/htdocs/index2.html /usr/local/apache2/htdocs/$GITHUB_ORGANIZATION/index.html
-  done
+  do_local_cleanup_for_old_github_organizations
 
   # loop over all the organization repo list 
   # and clone its repositories locally
+  rm -f /usr/local/apache2/htdocs/GITHUB_ORGANIZATIONS.txt && touch /usr/local/apache2/htdocs/GITHUB_ORGANIZATIONS.txt
   for GITHUB_ORGANIZATION in $GITHUB_ORGANIZATIONS
   do
+    # stuff for HTML view
+    echo $GITHUB_ORGANIZATION >> /usr/local/apache2/htdocs/GITHUB_ORGANIZATIONS.txt
+    mkdir -p /usr/local/apache2/htdocs/$GITHUB_ORGANIZATION/
+    cp -f /usr/local/apache2/htdocs/index2.html /usr/local/apache2/htdocs/$GITHUB_ORGANIZATION/index.html
 
     echo "-> Dumping the $GITHUB_ORGANIZATION github organization"
     get_github_clone_urls
